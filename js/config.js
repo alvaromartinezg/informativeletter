7e0e6d0acef964042cb64c683ce5c0fbe25d6f38
alvaro.martinez <alvandremg@gmail.com>
2025-11-20 16:17:30 -0500

    Update config.js

--- js/config.js
+++ js/config.js
@@ -4,4 +4,5 @@
 window.ENDPOINTS = {
   PROCESS_URL: "https://kmz-processor-411960240345.us-central1.run.app/process",
   CONVERT_URL: "https://kmz2cad-411960240345.us-central1.run.app/process"
+  
 };
